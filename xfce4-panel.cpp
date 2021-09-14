e3028b0ffa6bb9bf27fe75386c911216877c7990
Kirara17233 <Kirara17233@gmail.com>
2021-09-14 16:15:02 +0300

    a

--- xfce4-panel.cpp
+++ xfce4-panel.cpp
@@ -1,9 +1,23 @@
 #include "xfce4-panel.hpp"
+#include <limits>
 #include <iostream>
 #include <iomanip>
+#include <fstream>
+#include "config.hpp"
+#include "stream.hpp"
 #include "scheme.hpp"
 
 void xfce4_panel::change(const scheme::Scheme& scheme)
 {
-  std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(6) << (double)255 / 255;
+  std::fstream fs(config::xmobar);
+  for (int i = 0; i < 3; ++i)
+  {
+    if (stream::find("double", fs))
+    {
+      fs.ignore(std::numeric_limits<std::streamsize>::max(), '"');
+      fs.ignore(std::numeric_limits<std::streamsize>::max(), '"');
+      fs << std::setiosflags(std::ios::fixed) << std::setprecision(6) << (double)scheme[scheme::ColorType::BACKGROUND_COLOR][i] / 255;
+    }
+  }
+  fs.close();
 }
